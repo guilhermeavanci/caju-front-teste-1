2c90d3f5c81fdf7d9246a954b3618e66e9b6c53b
Guilherme Pires <guilhermeavancip@gmail.com>
2024-10-16 17:16:35 -0300

    build: adding axe a11y tool as suggested by eslint-plugin-jsx-a11y docs

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import './index.css'
+import reportAccessibility from "./utils/reportAccessibility";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+
+reportAccessibility(React)
