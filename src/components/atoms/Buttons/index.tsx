efda9c33643f08c9f43e06383c3b2a2fe44bd7bb
Guilherme Pires <guilhermeavancip@gmail.com>
2024-10-20 00:18:53 -0300

    fix: removing unused 'disabled' attr on button styles

--- src/components/atoms/Buttons/index.tsx
+++ src/components/atoms/Buttons/index.tsx
@@ -6,7 +6,6 @@ const hoverShadow = `0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0,
 
 type ButtonStyle = {
   $color?: Color
-  disabled?: boolean
   $preventAnimation?: boolean
   $rounded?: boolean
 }
