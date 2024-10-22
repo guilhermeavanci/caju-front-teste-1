d896302453c72ee0a9d78b11830cd21944adcf08
Guilherme Pires <guilhermeavancip@gmail.com>
2024-10-22 00:14:52 -0300

    chore: adding color theme to header

--- src/components/atoms/Header/index.tsx
+++ src/components/atoms/Header/index.tsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components'
+import { COLORS } from '~/theme'
 
 export const Header = styled.header`
-  background: rgb(255, 117, 0);
-  background: linear-gradient(258deg, rgba(255, 117, 0, 1) 8%, rgba(232, 5, 55, 1) 53%);
+  background: ${COLORS.SECONDARY};
+  background: linear-gradient(258deg, ${COLORS.SECONDARY} 8%, ${COLORS.TERTIARY} 53%);
   width: 100%;
   height: 64px;
   position: fixed;
